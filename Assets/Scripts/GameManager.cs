72b3cd0a0973c09506d0882c9d3b8c6467ac4323
cuecharles <cuecharles@gmail.com>
2020-04-29 17:13:00 -0700

    Fixed the targetting system bug

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public int score;
+    public float timeLeft;
+
+    public Target[] targets;
+    public Target chosenTarget;
+
+    void Start()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+
+            ChooseNewTarget();
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChooseNewTarget()
+    {
+        for(int i = 0; i < targets.Length; i++)
+        {
+            targets[i].isTarget = false;
+        }
+
+        chosenTarget = targets[Random.Range(0, targets.Length)];
+        chosenTarget.gameObject.tag = "ColourTarget";
+        chosenTarget.isTarget = true;
+
+
+    }
+
+
+}
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -4,47 +4,39 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    public GameObject[] targets;
     public bool isTarget;
-    public GameObject chosenTarget;
     public Material onTarget;
     public Material offTarget;
-    public bool hit;
 
     void Start()
     {
-        chosenTarget = targets[Random.Range(0, targets.Length)];
-        chosenTarget.transform.gameObject.tag = "ColourTarget";
-        isTarget = true;
-        hit = false;
+        
     }
 
     void Update()
     {
         if (isTarget == true)
         {
-            chosenTarget.transform.gameObject.tag = "ColourTarget";
+            gameObject.tag = "ColourTarget";
             gameObject.GetComponent<Renderer>().material = onTarget;
-            hit = false;
         }
         else
         {
-            chosenTarget.transform.gameObject.tag = "TargetsToBe";
+            gameObject.tag = "TargetsToBe";
             gameObject.GetComponent<Renderer>().material = offTarget;
         }
 
-        if (hit == true)
-        {
-            chosenTarget = targets[Random.Range(0, targets.Length)];
-        }
     }
 
     void OnCollisionEnter(Collision other)
     {
         if (other.transform.tag == "Object" && isTarget)
         {
+            print("YAY");
             isTarget = false;
-            hit = true;
+
+            GameManager.Instance.score += 5;
+            GameManager.Instance.ChooseNewTarget();
         }
     }
 }
